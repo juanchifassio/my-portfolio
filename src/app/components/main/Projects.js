45ea2bcfafde6844d0484c886c7a08fd6a42c2fc
unknown <juanchifassio@gmail.com>
2021-12-17 12:55:48 -0300

    updates

--- src/app/components/main/Projects.js
+++ src/app/components/main/Projects.js
@@ -32,19 +32,21 @@ const Projects = () => {
                             title: 'Crypto News App',
                             desc: 'Crypto App made with React, Chart.js, News API and CoinGecko Api. Styled with Chakra UI (still working on it! Demo soon 😊).',
                             demolink: '#',
-                            sourcelink: 'https://github.com/juanchifassio/MelonCrypto/tree/main/melon-crypto%20-%20copia',
+                            sourcelink:
+                                'https://github.com/juanchifassio/MelonCrypto/tree/main/melon-crypto%20-%20copia',
                             tag: 'Javascript - React'
                         },
                         {
                             title: 'Weather App',
                             desc: 'Weather App made with React, OpenWeather API and Geolocation API. Styled with Chakra UI and deployed with Firebase.',
                             demolink: 'https://weatherapp-939a1.web.app/',
-                            sourcelink: 'https://github.com/juanchifassio/Weather-App',
+                            sourcelink:
+                                'https://github.com/juanchifassio/Weather-App',
                             tag: 'Javascript - React'
                         },
                         {
                             title: 'CRM project',
-                            desc: 'Customer Relationship Management project, where you can manage your agents and assign them leads. This is my first Django project.',
+                            desc: 'Customer Relationship Management project, where you can manage your agents and assign them leads. Deployed with Heroku. This is my first Django project.',
                             demolink: 'https://crm-byjuanfassio.herokuapp.com/',
                             sourcelink:
                                 'https://github.com/juanchifassio/CRM/tree/master',
